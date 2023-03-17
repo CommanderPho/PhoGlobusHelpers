f00bbacb7c916b5c0b79636ed0d95c28e4d545e2
Pho Hale <PhoHale@gmail.com>
2023-03-17 17:51:45 -0400

    added sudo

--- scripts/mount_turbo.sh
+++ scripts/mount_turbo.sh
@@ -2,5 +2,5 @@ MOUNT_NAME=umms-kdiba
 STORAGE_VOLUME="${MOUNT_NAME}.turbo.storage.umich.edu:/${MOUNT_NAME}"
 MOUNT_LOCATION="${HOME}/cloud/turbo_umms_kdiba"
 mkdir -p "${MOUNT_LOCATION}"
-mount -t nfs "${STORAGE_VOLUME}" "${MOUNT_LOCATION}"
+sudo mount -t nfs "${STORAGE_VOLUME}" "${MOUNT_LOCATION}"
 
